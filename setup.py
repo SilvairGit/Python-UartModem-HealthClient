4a516bffe26e0c44d423efcad5182554b148eb5b
Przemysław Fierek <przemyslaw.fierek@silvair.com>
2018-08-30 09:12:47 +0200

    2.9.0 release

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name="silvair_health_client",
-    version="2.8.0",
+    version="2.9.0",
     author="Silvair",
     author_email="support@silvair.com",
     packages=find_packages(exclude=["tests"]),
     python_requires=">=3.6.0",
     install_requires=[
-        "silvair-uart-common-libs==2.8.0",
-        "silvair-otau-demo==2.8.0",
+        "silvair-uart-common-libs==2.9.0",
+        "silvair-otau-demo==2.9.0",
         "nose"
     ],
     dependency_links = [
-      "git+https://github.com/SilvairGit/Python-UartModem-Common.git@2.8.0#egg=silvair-uart-common-libs-2.8.0",
-      "git+https://github.com/SilvairGit/Python-UartModem-OTAU.git@2.8.0#egg=silvair-otau-demo-2.8.0"
+      "git+https://github.com/SilvairGit/Python-UartModem-Common.git@2.9.0#egg=silvair-uart-common-libs-2.9.0",
+      "git+https://github.com/SilvairGit/Python-UartModem-OTAU.git@2.9.0#egg=silvair-otau-demo-2.9.0"
     ]
 )
